56bbf879d19fd71be419a69ca45ec0da20edc80d
Aditya2102 <aadisonune999@gmail.com>
2026-04-01 20:04:34 +0530

    Add files via upload

---
+++ frstprogram.py
@@ -0,0 +1,91 @@
+
+# name = "aditya"
+# age=22
+# marks=90.5
+# old= True
+# a= None
+
+# print(type(name))
+# print(type(age))
+# print (type(marks))
+# print(type(old))
+# print(type(a))
+ 
+
+# print sum 
+# a=10
+# b=40
+# sum=a + b
+# print("the sum of a and b is", sum)
+
+
+
+#omment = "this is a comment"
+
+
+#operators
+# +
+# -
+# *
+# /
+#%
+#**
+# a=10
+# b=20
+# sum = a ** b
+# print(sum)
+
+# relational operators
+# a = 5
+# b = 10
+# print(a == b)
+# print(a != b)
+# print (a >= b)
+# print (a <= b )
+
+#assignment operators
+# a = 10
+# a /= 5
+# print(a)
+
+#logical operators
+# val1 = True
+# val2 = False
+# print(val1 or val2)   
+
+
+#type conversion
+ 
+# a = "10"
+# b = int (a)
+# print (type(b))
+
+# c = 10
+# d = str (c)
+# print (type(d))
+
+
+
+#input function
+
+# name= input("enter your name ")
+# age = int (input("enter your age "))
+# marks = float (input ("enter your marks "))
+
+# practice 
+
+# first = int (input(" enter first number"))
+# second = int (input ("enter second number"))
+# sum = first + second
+# print("sum of first and second is ", sum)
+
+
+#2
+
+# a = int(input("enter first number"))
+
+# b = int (input ("enter second number"))
+
+# print (a >= b)
+
+
